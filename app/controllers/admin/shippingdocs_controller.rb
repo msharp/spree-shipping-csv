c39d8891e779927e2efe8c6d79827b48d5da66fa
max sharples <maxsharples@gmail.com>
2011-11-09 16:20:32 +1100

    specific output format for client

--- app/controllers/admin/shippingdocs_controller.rb
+++ app/controllers/admin/shippingdocs_controller.rb
@@ -22,87 +22,75 @@ class Admin::ShippingdocsController < Admin::BaseController
     csv_string = FasterCSV.generate do |csv|
         # header row
         csv << [
-            "order_number", 
-            "order_date", 
-            "order_total", 
-            "billing_first_name", 
-            "billing_last_name", 
-            "billing_address_1", 
-            "billing_address_2", 
-            "billing_city", 
-            "billing_state", 
-            "billing_zip",
-            "shipping_first_name", 
-            "shipping_last_name", 
-            "shipping_address_1", 
-            "shipping_address_2", 
-            "shipping_city", 
-            "shipping_state",
-            "shipping_zip", 
-            "weight", 
-            "email", 
-            "payment_state", 
-            "shipment_state", 
-            "special_instructions", 
-            "products_ordered" 
+            "Order",
+            "DateCompleted",
+            "ProductSKU",
+            "ProductName",
+            "Quantity",
+            "Units",
+            "Price",
+            "SubTotal",
+            "Refund",
+            "RefundReason",
+            "CartStatus",
+            "WineBrand",
+            "ThirdPartyStore",
+            "ShipFirstName",
+            "ShipLastName",
+            "ShipCompany",
+            "ShipAddress",
+            "ShipAddress2",
+            "ShipCity",
+            "ShipState",
+            "ShipZip",
+            "ShipPhone",
+            "ShipEmail"
         ]
       
-      # data rows
-      @orders.each do |order|
+        # data rows
+        @orders.each do |order|
+        
+            for item in order.line_items do 
+                
+                csv_line = [
+                    order.number, 
+                    order.completed_at,
+                    item.variant.sku,
+                    item.variant.product.name,
+                    "1", # 1 line in output per item ordered,
+                    "6", # wineplus hack - all products contain 6 units,
+                    item.variant.price,
+                    item.variant.price, # 'subtotal',
+                    "",
+                    "",
+                    "Complete",
+                    item.variant.product.name,
+                    "WinePlus",
+                    order.ship_address.firstname, 
+                    order.ship_address.lastname, 
+                    "",
+                    order.ship_address.address1, 
+                    order.ship_address.address2, 
+                    order.ship_address.city ,
+                    State.find(:first, :conditions => { :id => order.ship_address.state_id}).abbr, 
+                    order.ship_address.zipcode, 
+                    "",
+                    order.email
+                ]
 
-        if order.state == 'in_progress' then
-        else
-          @shipmentWeight = 0
-          order.line_items.each do |item|
-            if item.variant.weight.nil? then
-            else
-              @shipmentWeight += item.variant.weight
-            end
-          end
-          csv << [
-              order.number, 
-              order.completed_at, 
-              order.total, 
-              order.bill_address.firstname, 
-              order.bill_address.lastname, 
-              order.bill_address.address1, 
-              order.bill_address.address2, 
-              order.bill_address.city,
-              State.find(:first, :conditions => { :id =>order.bill_address.state_id}).abbr, 
-              order.bill_address.zipcode,
-              order.ship_address.firstname, 
-              order.ship_address.lastname, 
-              order.ship_address.address1, 
-              order.ship_address.address2, 
-              order.ship_address.city ,
-              State.find(:first, :conditions => { :id => order.ship_address.state_id}).abbr, 
-              order.ship_address.zipcode, 
-              @shipmentWeight, 
-              order.email, 
-              order.payment_state, 
-              order.shipment_state, 
-              order.special_instructions, 
-              ordered_items(order)
-          ]
-            
+
+                item.quantity.times do
+                    csv << csv_line
+                end
+            end     
         end
-
-       end
     end
 
-      # send it to the browsah
-      send_data csv_string,
+    # send it to the browsah
+    send_data csv_string,
             :type => 'text/csv; charset=iso-8859-1; header=present',
             :disposition => "attachment; filename=users.csv"
-  end
 
-  def ordered_items(order)
-    items = ""
-    for item in order.line_items do 
-      items << "#{item.variant.sku} #{item.variant.product.name} #{item.variant.options_text} (#{item.quantity})
-"             
-    end
-    items
   end
 
 end
